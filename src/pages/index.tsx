32713b098ba59ce5737422ba3b891f91c1f54f64
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-07-24 01:48:39 +0900

    feat: add mockup UI for todo list

---
+++ src/pages/index.tsx
@@ -0,0 +1,50 @@
+import { Box, Button, Checkbox, Divider, FormControl, Heading, HStack, Input, VStack } from "@chakra-ui/react";
+import { useState } from "react";
+
+type Todo = {
+    id: number;
+    title: string;
+    completed: boolean;
+};
+
+const todos: Todo[] = [
+    { id: 1, title: "todo1", completed: false },
+    { id: 2, title: "todo2", completed: false },
+    { id: 3, title: "todo3", completed: true },
+];
+export default function Home() {
+    const [inputValue, setInputValue] = useState<string>("");
+
+    return (
+        <Box w="100vw" h="100vh" bgColor="gray.50" p={20}>
+            <VStack align="start" spacing={2}>
+                <Heading>Todo</Heading>
+                <VStack divider={<Divider />} spacing={0} borderRadius={6} bgColor="gray.100" w="100%">
+                    {todos.map((todo, i) => (
+                        <HStack key={i} justify="space-between" p={2} w="100%" _hover={{ bgColor: "whiteAlpha.700" }}>
+                            <Checkbox isChecked={todo.completed} onChange={() => console.log("update complete")} size="md">
+                                {todo.title}
+                            </Checkbox>
+                            <Button onClick={() => console.log("delete")} colorScheme="red" size="sm">
+                                Delete
+                            </Button>
+                        </HStack>
+                    ))}
+                </VStack>
+                <FormControl>
+                    <HStack>
+                        <Input
+                            value={inputValue || ""}
+                            onChange={(e) => setInputValue(e.target.value)}
+                            placeholder="Input todo"
+                            size="sm"
+                        ></Input>
+                        <Button colorScheme="green" size="sm" onClick={() => console.log("submit")}>
+                            Submit
+                        </Button>
+                    </HStack>
+                </FormControl>
+            </VStack>
+        </Box>
+    );
+}
