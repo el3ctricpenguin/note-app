e7c85f003a76aa0928ad008e988b7107c5b1d186
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-07-26 01:42:01 +0900

    feat: add api todo (update)

---
+++ src/pages/api/todo/[id].ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient, Todo } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { id } = req.query;
+    switch (req.method) {
+        case "GET": {
+            const todo: Todo | null = await prisma.todo.findUnique({
+                where: { id: Number(id) },
+            });
+            res.json(todo);
+        }
+        case "PATCH": {
+            const todo: Todo | null = await prisma.todo.findUnique({
+                where: { id: Number(id) },
+            });
+            const todoEdited: Todo = await prisma.todo.update({
+                where: { id: Number(id) },
+                data: { completed: !todo?.completed },
+            });
+            res.json(todoEdited);
+        }
+    }
+}
