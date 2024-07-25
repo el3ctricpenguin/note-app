e59db2266c3622b518a4920fc02ca3e67c5c562f
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-07-26 02:50:08 +0900

    feat: add createTodo (handleSubmit)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,42 @@
 import { Box, Button, Checkbox, Divider, FormControl, Heading, HStack, Input, VStack } from "@chakra-ui/react";
 import { Todo } from "@prisma/client";
-import { useEffect, useState } from "react";
+import { create } from "domain";
+import { FormEvent, useEffect, useState } from "react";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export default function Home() {
     const [inputValue, setInputValue] = useState<string>("");
     const [todos, setTodos] = useState<Todo[]>([]);
 
+    const fetchTodos = async () => {
+        const response = await fetch(`${apiUrl}/todo`);
+        const todos = await response.json();
+        console.log(todos);
+        setTodos(todos);
+    };
+
     useEffect(() => {
-        const fetchTodos = async () => {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/todo`);
-            const todos = await response.json();
-            console.log(todos);
-            setTodos(todos);
-        };
         fetchTodos();
     }, []);
+
+    const createTodo = async (title: string) => {
+        console.log(`create ${title}`);
+        const response = await fetch(`${apiUrl}/todo`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ title }),
+        });
+        return await response.json();
+    };
+
+    const handleSubmit = async () => {
+        await createTodo(inputValue);
+        await fetchTodos();
+        setInputValue("");
+    };
 
     return (
         <Box w="100vw" h="100vh" bgColor="gray.50" p={20}>
@@ -39,8 +61,8 @@ export default function Home() {
                             onChange={(e) => setInputValue(e.target.value)}
                             placeholder="Input todo"
                             size="sm"
-                        ></Input>
-                        <Button colorScheme="green" size="sm" onClick={() => console.log("submit")}>
+                        />
+                        <Button colorScheme="green" size="sm" type="submit" onClick={handleSubmit}>
                             Submit
                         </Button>
                     </HStack>
