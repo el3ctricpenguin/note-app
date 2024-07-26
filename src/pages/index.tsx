1c2560538c6ee292918975cf6c3266da41cfea8a
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-07-27 02:25:33 +0900

    feat: add Home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,5 @@
+import Home from "@/routes";
+
+export default function Index() {
+    return <Home />;
+}
---
+++ src/routes/index.tsx
@@ -0,0 +1,38 @@
+import { Badge, Box, Card, CardBody, Heading, HStack, Image, LinkBox, LinkOverlay, Text } from "@chakra-ui/react";
+
+export default function Home() {
+    return (
+        <Box w="100vw" h="100vh" bgColor="gray.50" p={{ base: 5, sm: 10, md: 20 }}>
+            <Heading my={4} as="h1" size="2xl">
+                note-app
+            </Heading>
+            <LinkBox>
+                <Card variant="elevated" direction={{ base: "column", sm: "row" }} borderRadius={8} overflow="hidden">
+                    <Image
+                        src="https://www.timedoctor.com/blog/images/2020/10/task-list-template-1024x576.jpg"
+                        alt="task list"
+                        aspectRatio="2/1"
+                        objectFit="cover"
+                        maxW={{ base: "100%", sm: "200px" }}
+                    />
+                    <CardBody>
+                        <LinkOverlay href="/todo">
+                            <Text fontSize="2xl" fontWeight="bold">
+                                TODO
+                            </Text>
+                        </LinkOverlay>
+                        <Text>DBを使用した簡単なチェックリスト</Text>
+                    </CardBody>
+                    <HStack position="absolute" left={2} top={2}>
+                        <Badge colorScheme="red" variant="solid" fontSize="sm">
+                            New
+                        </Badge>
+                        <Badge colorScheme="blue" variant="solid" fontSize="sm">
+                            In progress
+                        </Badge>
+                    </HStack>
+                </Card>
+            </LinkBox>
+        </Box>
+    );
+}
