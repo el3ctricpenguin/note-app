0c9e363466bdbcf5f23d06fc933d8e1907be646f
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-07-27 03:19:02 +0900

    feat: add color mode switcher

--- src/components/layout/BasicLayout.tsx
+++ src/components/layout/BasicLayout.tsx
@@ -1,13 +1,23 @@
-import { Box, Heading } from "@chakra-ui/react";
-import Link from "next/link";
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { Box, Heading, HStack, IconButton, useColorMode } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { ReactElement } from "react";
 
 export default function BasicLayout({ children }: { children: ReactElement }) {
+    const { colorMode, toggleColorMode } = useColorMode();
     return (
-        <Box w="100vw" h="100vh" bgColor="gray.50" p={{ base: 5, sm: 10, md: 20 }}>
-            <Heading my={4} as="h1" size="2xl">
-                <Link href="/">note-app</Link>
-            </Heading>
+        <Box w="100vw" h="100vh" bgColor="brand.gray.100" p={{ base: 5, sm: 10, md: 20 }}>
+            <HStack justify="space-between">
+                <Heading my={4} as="h1" size="2xl">
+                    <NextLink href="/">note-app</NextLink>
+                </Heading>
+                <IconButton
+                    aria-label={"Colormode Switcher"}
+                    icon={colorMode == "light" ? <SunIcon /> : <MoonIcon />}
+                    onClick={toggleColorMode}
+                    size="sm"
+                />
+            </HStack>
             {children}
         </Box>
     );
