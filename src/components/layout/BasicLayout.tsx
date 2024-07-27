f92088fe999d30b5c49cfb9345439990e9fcd6c1
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-07-27 14:57:47 +0900

    feat: add two stars

--- src/components/layout/BasicLayout.tsx
+++ src/components/layout/BasicLayout.tsx
@@ -1,5 +1,5 @@
-import { MoonIcon, SunIcon } from "@chakra-ui/icons";
-import { Box, Heading, HStack, IconButton, useColorMode } from "@chakra-ui/react";
+import { MoonIcon, StarIcon, SunIcon } from "@chakra-ui/icons";
+import { Box, Divider, Heading, HStack, IconButton, useColorMode, VStack } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { ReactElement } from "react";
 
@@ -8,7 +8,7 @@ export default function BasicLayout({ children }: { children: ReactElement }) {
     return (
         <Box w="100vw" h="100vh" bgColor="brand.gray.50" p={{ base: 5, sm: 10, md: 20 }}>
             <HStack my={4} justify="space-between">
-                <Heading as="h1" size="2xl">
+                <Heading as="h1" size="2xl" color="brand.gray.900">
                     <NextLink href="/">note-app</NextLink>
                 </Heading>
                 <IconButton
@@ -20,6 +20,13 @@ export default function BasicLayout({ children }: { children: ReactElement }) {
                 />
             </HStack>
             {children}
+            <VStack mt={4}>
+                <Divider borderColor="brand.gray.900" />
+                <HStack spacing={0.5} justify="end" w="100%">
+                    <StarIcon w={3} />
+                    <StarIcon w={3} />
+                </HStack>
+            </VStack>
         </Box>
     );
 }
