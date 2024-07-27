d906b185b433f77714b678a324d7432df5fce8b7
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-07-27 17:00:16 +0900

    fix: add key to Stars

--- src/components/Cards/FilmRating.tsx
+++ src/components/Cards/FilmRating.tsx
@@ -4,7 +4,7 @@ import { HStack } from "@chakra-ui/react";
 export const FilmRating = ({ rating }: { rating: number }) => {
     const stars = [];
     for (let i = 0; i < 5; i++) {
-        stars.push(i < rating ? <StarIcon w={4} /> : <StarIcon w={4} opacity={0.5} />);
+        stars.push(i < rating ? <StarIcon w={4} key={i} /> : <StarIcon w={4} opacity={0.5} key={i} />);
     }
     return <HStack w="100%">{stars}</HStack>;
 };
