0c2a384210e2a20bc5ab1bb87fee8f81c2b75aef
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-07-27 14:49:53 +0900

    feat: modularize index card

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,71 @@
-import { Badge, Box, Card, CardBody, Heading, HStack, Image, LinkBox, LinkOverlay, Text } from "@chakra-ui/react";
+import { Badge, Box, Card, CardBody, Heading, HStack, Image, LinkBox, LinkOverlay, SimpleGrid, Text, VStack } from "@chakra-ui/react";
 import NextLink from "next/link";
 
 export default function Home() {
+    const apps = [
+        {
+            name: "TODO",
+            description: "DBを使用した簡単なチェックリスト",
+            href: "/todo",
+            imageURL: "https://www.timedoctor.com/blog/images/2020/10/task-list-template-1024x576.jpg",
+            isNew: true,
+            isInProgress: true,
+        },
+        {
+            name: "film-note",
+            description: "見た映画を記録するアプリ",
+            href: "/film",
+            imageURL: "https://kettleheroes.com/wp-content/uploads/2022/02/Why-is-Popcorn-a-Movie-Food.jpg",
+            isNew: true,
+            isInProgress: true,
+        },
+    ];
     return (
         <>
-            <LinkBox>
-                <Card variant="elevated" direction={{ base: "column", sm: "row" }} borderRadius={8} overflow="hidden">
-                    <Image
-                        src="https://www.timedoctor.com/blog/images/2020/10/task-list-template-1024x576.jpg"
-                        alt="task list"
-                        aspectRatio="2/1"
-                        objectFit="cover"
-                        maxW={{ base: "100%", sm: "200px" }}
-                    />
-                    <CardBody>
-                        <LinkOverlay href="/todo" as={NextLink}>
-                            <Text fontSize="2xl" fontWeight="bold">
-                                TODO
-                            </Text>
-                        </LinkOverlay>
-                        <Text>DBを使用した簡単なチェックリスト</Text>
-                    </CardBody>
-                    <HStack position="absolute" left={2} top={2}>
-                        <Badge colorScheme="red" variant="solid" fontSize="sm">
-                            New
-                        </Badge>
-                        <Badge colorScheme="blue" variant="solid" fontSize="sm">
-                            In progress
-                        </Badge>
-                    </HStack>
-                </Card>
-            </LinkBox>
+            <SimpleGrid minChildWidth="sm" spacing={4}>
+                {apps.map((app, i) => {
+                    return (
+                        <Card
+                            as={LinkBox}
+                            variant="elevated"
+                            direction={{ base: "column", sm: "row" }}
+                            borderRadius={8}
+                            overflow="hidden"
+                            key={i}
+                            minH="120px"
+                        >
+                            <Image
+                                src={app.imageURL}
+                                alt="task list"
+                                aspectRatio="2/1"
+                                objectFit="cover"
+                                w={{ base: "100%", sm: "40%" }}
+                                maxW={{ base: "100%", sm: "200px" }}
+                            />
+                            <CardBody>
+                                <LinkOverlay href={app.href} as={NextLink}>
+                                    <Text fontSize="2xl" fontWeight="bold">
+                                        {app.name}
+                                    </Text>
+                                </LinkOverlay>
+                                <Text>{app.description}</Text>
+                            </CardBody>
+                            <HStack position="absolute" left={2} top={2}>
+                                {app.isNew && (
+                                    <Badge color="white" bgColor="red.500" fontSize="sm">
+                                        New
+                                    </Badge>
+                                )}
+                                {app.isInProgress && (
+                                    <Badge color="white" bgColor="blue.500" fontSize="sm">
+                                        In progress
+                                    </Badge>
+                                )}
+                            </HStack>
+                        </Card>
+                    );
+                })}
+            </SimpleGrid>
         </>
     );
 }
