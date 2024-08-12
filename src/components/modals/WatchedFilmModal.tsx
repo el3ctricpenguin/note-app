edcdd12271d803eb691dcb3596bce32a9219cf53
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-08-12 21:12:32 +0900

    feat: add Editable Textarea to edit note of WatchedFilm

--- src/components/modals/WatchedFilmModal.tsx
+++ src/components/modals/WatchedFilmModal.tsx
@@ -1,21 +1,61 @@
-import { Skeleton, Image, Table, TableContainer, Tbody, Td, Tr, Checkbox, Text, HStack, Link, VStack, Tooltip } from "@chakra-ui/react";
+import {
+    Skeleton,
+    Image,
+    Table,
+    TableContainer,
+    Tbody,
+    Td,
+    Tr,
+    Text,
+    HStack,
+    Link,
+    VStack,
+    Tooltip,
+    Editable,
+    EditablePreview,
+    EditableTextarea,
+    ButtonGroup,
+    Flex,
+    IconButton,
+    useEditableControls,
+    useToast,
+} from "@chakra-ui/react";
 import { BasicModal } from "./BasicModal";
 import { apiUrl, TMDB_API_KEY } from "@/config";
 import { TMDB_API_URL, TMDB_FILM_PAGE_URL, TMDB_IMAGE_API_URL_MD } from "@/config/constants";
 import { useCallback, useEffect, useState } from "react";
 import { WatchedFilm } from "@prisma/client";
-import { AttachmentIcon, BellIcon, RepeatClockIcon, CheckCircleIcon, NotAllowedIcon, ExternalLinkIcon, ViewIcon } from "@chakra-ui/icons";
+import { AttachmentIcon, BellIcon, RepeatClockIcon, ExternalLinkIcon, EditIcon, CheckIcon, CloseIcon } from "@chakra-ui/icons";
 import getFlagEmoji from "@/features/utils/getFlagEmoji";
 import dayjs from "dayjs";
 import { FilmRating } from "../cards/FilmRating";
+
+function EditableControls() {
+    const { isEditing, getSubmitButtonProps, getCancelButtonProps, getEditButtonProps } = useEditableControls();
+
+    return isEditing ? (
+        <ButtonGroup justifyContent="center" size="sm">
+            <IconButton icon={<CheckIcon />} {...getSubmitButtonProps()} aria-label="submit" />
+            <IconButton icon={<CloseIcon />} {...getCancelButtonProps()} aria-label="cancel" />
+        </ButtonGroup>
+    ) : (
+        <Flex justifyContent="center">
+            <IconButton size="sm" icon={<EditIcon />} {...getEditButtonProps()} aria-label="edit" />
+        </Flex>
+    );
+}
+
 interface WatchedFilmModalProps {
     watchedFilmId: number;
     isOpen: boolean;
     onClose: () => void;
 }
+
 export const WatchedFilmModal = ({ watchedFilmId, isOpen, onClose }: WatchedFilmModalProps) => {
     const [filmData, setFilmData] = useState<any>();
     const [watchedFilm, setWatchedFilm] = useState<WatchedFilm>();
+    const [isImgLoaded, setIsImgLoaded] = useState(false);
+
     const fetchWatchedFilm = useCallback(async () => {
         const response = await fetch(`${apiUrl}/film/watched/${watchedFilmId}`, { method: "GET" });
         const watchedFilm = await response.json();
@@ -37,7 +77,53 @@ export const WatchedFilmModal = ({ watchedFilmId, isOpen, onClose }: WatchedFilm
             fetchWatchedFilm();
         }
     }, [watchedFilmId, fetchWatchedFilm, watchedFilm, fetchFilmData]);
-    const [isImgLoaded, setIsImgLoaded] = useState(false);
+
+    const [note, setNote] = useState("");
+    const toast = useToast();
+    const updateWatchedFilm = async (
+        watchedFilmId: string,
+        filmId?: string,
+        watchedDate?: string,
+        rating?: number,
+        note?: string
+    ): Promise<[any, number]> => {
+        console.log(`update watched film: ${filmId}`);
+        const isoWatchedDate = dayjs(watchedDate).toISOString();
+        console.log(note);
+
+        const response = await fetch(`${apiUrl}/film/watched/${watchedFilmId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ note: note }),
+        });
+        return [await response.json(), response.status];
+    };
+    const handleEditNoteSubmit = async () => {
+        const [response, status] = await updateWatchedFilm(watchedFilmId.toString(), undefined, undefined, undefined, note);
+        console.log(response);
+
+        if (status == 201) {
+            toast({
+                title: "film updated",
+                status: "success",
+                duration: 3000,
+                isClosable: true,
+            });
+            await fetchWatchedFilm();
+        }
+        if (status == 500) {
+            toast({
+                title: "film update failed",
+                description: `${response.error}`,
+                status: "error",
+                duration: 3000,
+                isClosable: true,
+            });
+        }
+    };
+
     return (
         <BasicModal title="" isOpen={isOpen} onClose={onClose}>
             <Skeleton isLoaded={!filmData || isImgLoaded}>
@@ -99,8 +185,19 @@ export const WatchedFilmModal = ({ watchedFilmId, isOpen, onClose }: WatchedFilm
                                     <AttachmentIcon mr={2} />
                                     メモ
                                 </Td>
-                                <Td px={0} pl={4} whiteSpace="pre-line" py={3}>
-                                    {watchedFilm?.note}
+                                <Td px={0} pl={4} whiteSpace="pre-line" py={3} w="full">
+                                    <Editable
+                                        defaultValue={watchedFilm?.note ? watchedFilm?.note : ""}
+                                        onSubmit={handleEditNoteSubmit}
+                                        submitOnBlur={false}
+                                        // onBlurで一時的な値を保存 (submitしない)してチェックボタン押した時だけ送信するように変更
+                                    >
+                                        <HStack>
+                                            <EditablePreview />
+                                            <EditableTextarea onChange={(e) => setNote(e.target.value)} />
+                                            <EditableControls />
+                                        </HStack>
+                                    </Editable>
                                 </Td>
                             </Tr>
                         </Tbody>
