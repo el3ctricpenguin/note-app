9bae1c51745dc22220be2af2dd301652b97549e1
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2024-08-12 21:12:04 +0900

    feat: add API for updating WatchedFilm

--- src/pages/api/film/watched/[watchedFilmId].ts
+++ src/pages/api/film/watched/[watchedFilmId].ts
@@ -4,28 +4,31 @@ import { PrismaClient, WatchedFilm } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { watchedFilmId } = req.query;
     switch (req.method) {
         case "GET":
-            const { watchedFilmId } = req.query;
             const watchedFilm: WatchedFilm | null = await prisma.watchedFilm.findUnique({ where: { id: Number(watchedFilmId) } });
             res.json(watchedFilm);
             break;
-        // case "POST":
-        //     try {
-        //         const { filmId, watchedDate, rating, note } = req.body;
-        //         const watchedFilm: WatchedFilm = await prisma.watchedFilm.create({
-        //             data: {
-        //                 filmId,
-        //                 watchedDate,
-        //                 rating,
-        //                 note,
-        //             },
-        //         });
-        //         res.status(201).json(watchedFilm);
-        //     } catch (error) {
-        //         console.log(error);
-        //         res.status(500).json({ error: "Failed to create record" });
-        //     }
-        //     break;
+        case "PUT":
+            try {
+                const { filmId, watchedDate, rating, note } = req.body;
+                const watchedFilm: WatchedFilm = await prisma.watchedFilm.update({
+                    where: {
+                        id: Number(watchedFilmId),
+                    },
+                    data: {
+                        filmId,
+                        watchedDate,
+                        rating,
+                        note,
+                    },
+                });
+                res.status(201).json(watchedFilm);
+            } catch (error) {
+                console.log(error);
+                res.status(500).json({ error: "Failed to create record" });
+            }
+            break;
     }
 }
