58beab008feb1114af94d363d753e20de3b76161
el3ctricpenguin <el3ctricpenguin@protonmail.com>
2025-04-22 02:37:09 +0700

    feat: add sign-in fe/auth

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,15 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import { object, string, ZodError } from "zod";
+import { getUserFromDB } from "@/utils/auth";
+
+const signInSchema = object({
+    username: string({ required_error: "Username is required" }).min(1, "Username is required"),
+    password: string({ required_error: "Password is required" })
+        .min(1, "Password is required")
+        .min(8, "Password must be more than 8 characters")
+        .max(32, "Password must be less than 32 characters"),
+});
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [
@@ -7,6 +17,26 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
             credentials: {
                 username: {},
                 password: {},
+            },
+            async authorize(credentials, req) {
+                try {
+                    let user = null;
+                    const { username, password } = await signInSchema.parseAsync(credentials);
+                    user = await getUserFromDB(username, password);
+
+                    if (!user) {
+                        throw new Error("Invalid username or password");
+                    }
+
+                    return { ...user, id: user.id.toString() };
+                } catch (error) {
+                    if (error instanceof ZodError) {
+                        console.error("Zod validation error:", error);
+                        return null;
+                    }
+                    console.error("Authentication error:", error);
+                    return null;
+                }
             },
         }),
     ],
---
+++ src/pages/sign-in/index.tsx
@@ -0,0 +1,73 @@
+import { Button, FormControl, FormLabel, Heading, Input, VStack, FormErrorMessage } from "@chakra-ui/react";
+import { signIn } from "next-auth/react";
+import { useToast } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const signInSchema = z.object({
+    username: z.string().min(1, "ユーザー名は必須です"),
+    password: z.string().min(8, "パスワードは8文字以上である必要があります").max(32, "パスワードは32文字以下である必要があります"),
+});
+
+type FormData = z.infer<typeof signInSchema>;
+
+export default function SignIn() {
+    const toast = useToast();
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+    } = useForm<FormData>({
+        resolver: zodResolver(signInSchema),
+    });
+
+    const credentialsAction = async (data: FormData) => {
+        const result = await signIn("credentials", {
+            username: data.username,
+            password: data.password,
+            redirect: false,
+        });
+
+        console.log("SignIn result:", result);
+
+        if (result?.error) {
+            let message = "ログインに失敗しました";
+            if (result.error === "CredentialsSignin") {
+                message = "ユーザー名またはパスワードが正しくありません";
+            }
+            toast({
+                title: message,
+                status: "error",
+            });
+        } else {
+            toast({
+                title: "ログインに成功しました",
+                status: "success",
+            });
+        }
+    };
+
+    return (
+        <>
+            <Heading size="xl" mb={4}>
+                /sign-in
+            </Heading>
+            <FormControl as="form" onSubmit={handleSubmit(credentialsAction)}>
+                <VStack spacing={4} align="start">
+                    <FormControl id="credentials-username" isInvalid={!!errors.username}>
+                        <FormLabel fontSize="lg">ユーザー名</FormLabel>
+                        <Input type="text" {...register("username")} />
+                        <FormErrorMessage>{errors.username?.message}</FormErrorMessage>
+                    </FormControl>
+                    <FormControl id="credentials-password" isInvalid={!!errors.password}>
+                        <FormLabel fontSize="lg">パスワード</FormLabel>
+                        <Input type="password" {...register("password")} />
+                        <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
+                    </FormControl>
+                    <Button type="submit">ログイン</Button>
+                </VStack>
+            </FormControl>
+        </>
+    );
+}
